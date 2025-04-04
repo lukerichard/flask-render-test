c8f08ea0c1a725174d0d2f3f05591209e613e89a
lukerichard <lukerichard.p@gmail.com>
2025-04-04 16:25:09 -0400

    Update application design with new brand color and improve static file serving

--- app.py
+++ app.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
-app.debug = False  # Disable debug mode in production
+app.config['DEBUG'] = True
 
 @app.route('/')
 def home():
     return render_template('landing.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='127.0.0.1', port=5000) 
